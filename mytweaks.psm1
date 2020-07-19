975b1f20301cdf3940e17ffca552353e2dc7eb3c
Øyvind Helgeland <helgeland.oyvind@gmail.com>
2020-07-19 12:14:09 +0200

    Added choco install

--- mytweaks.psm1
+++ mytweaks.psm1
@@ -10,6 +10,11 @@ Function SetKeyboardDefault {
     Set-ItemProperty -Path "HKCU:\Control Panel\Keyboard" -Name "KeyboardSpeed" -Type String -Value 25
 }
 
+Function InstallChocolatey {
+    Set-ExecutionPolicy Bypass -Scope Process
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
 Function InstallStuff {
     winget install Postman.Postman
 }
